d38c5c4afc0c943e0367743adeb59aba1b58bc64
BuildTools <unconfigured@null.spigotmc.org>
2021-12-05 17:34:51 -0500

    Added start.js

---
+++ start.js
@@ -0,0 +1,15 @@
+const
+    path = require('path'),
+    fs = require('fs'),
+    https = require('http'),
+    app = require('express')().use(require('express').static(path.normalize(__dirname + '/'), {extensions: ['html']})).use((req, res) => {
+        res.status(404, res.send(fs.readFileSync(path.normalize(__dirname + '/404/index.html'), 'utf-8')))
+    }),
+    ssl = {
+        key: fs.readFileSync(path.join(__dirname, '/corrosion/ssl.key')),
+        cert: fs.readFileSync(path.join(__dirname, '/corrosion/ssl.cert')),
+    },
+    server = https.createServer(ssl, app);
+
+server.listen(8080);
+console.log('Server Running :D')
